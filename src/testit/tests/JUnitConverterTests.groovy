79431381689f2e544440280c2d65d178828ed9e5
crichmond1989 <crichmond1989@gmail.com>
2018-05-14 19:03:09 -0500

    removed compilestatic from JUnitConverterTests

--- src/testit/tests/JUnitConverterTests.groovy
+++ src/testit/tests/JUnitConverterTests.groovy
@@ -1,7 +1,5 @@
 package testit.tests
 
-import groovy.transform.CompileStatic
-
 import java.text.DecimalFormat
 import java.util.Date
 
@@ -19,7 +17,6 @@ import org.junit.Assert
 
 // JUnit 4 spec: http://llg.cubic.org/docs/junit/
 
-@CompileStatic
 class JUnitConverterTests implements Serializable {
     JUnitConverter converter = new JUnitConverter()
 
