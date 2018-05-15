da1f0b5e4c5cc00e25cf2ceb75881d85fc5daa32
crichmond1989 <crichmond1989@gmail.com>
2018-05-14 19:27:38 -0500

    even more noncps

--- src/testit/TestRunResult.groovy
+++ src/testit/TestRunResult.groovy
@@ -2,18 +2,24 @@ package testit
 
 import testit.SuiteResult
 
+@Grab("com.cloudbees:groovy-cps:1.1")
+import com.cloudbees.groovy.cps.NonCPS
+
 class TestRunResult implements Serializable {
     String name
     List<SuiteResult> suites
 
+    @NonCPS
     int getErrorCount() {
         return (int) suites?.collect { it.getErrorCount() }.sum() ?: 0
     }
 
+    @NonCPS
     int getFailureCount() {
         return (int) suites?.collect { it.getFailureCount() }.sum() ?: 0
     }
 
+    @NonCPS
     int getSuccessCount() {
         return (int) suites?.collect { it.getSuccessCount() }.sum() ?: 0
     }
