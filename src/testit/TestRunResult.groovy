b6bb1efb442abd5624a0a1ae26e0ab57c3175f3c
crichmond1989 <crichmond1989@gmail.com>
2018-05-16 13:51:10 -0500

    just methods?

--- src/testit/TestRunResult.groovy
+++ src/testit/TestRunResult.groovy
@@ -4,19 +4,21 @@ import groovy.transform.CompileStatic
 
 import testit.SuiteResult
 
-@CompileStatic
 class TestRunResult implements Serializable {
     String name
     List<SuiteResult> suites
 
+    @CompileStatic
     int getErrorCount() {
         return (int) suites?.collect { it.getErrorCount() }.sum() ?: 0
     }
 
+    @CompileStatic
     int getFailureCount() {
         return (int) suites?.collect { it.getFailureCount() }.sum() ?: 0
     }
 
+    @CompileStatic
     int getSuccessCount() {
         return (int) suites?.collect { it.getSuccessCount() }.sum() ?: 0
     }
