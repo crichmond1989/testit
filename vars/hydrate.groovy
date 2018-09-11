fb7aee73dff77907abebe6ba66689c444b4ad0dc
crichmond1989 <crichmond1989@gmail.com>
2018-09-11 15:14:21 -0500

    added init hydrate

---
+++ vars/hydrate.groovy
@@ -0,0 +1,20 @@
+List call(Map args) {
+    args = args ?: [:]
+
+    final classNames = args.classNames ?: findFiles(glob: "**/tests/*Tests.groovy").collect { it.name - ".groovy" }
+    final libraryName = args.library
+    final namespace = args.namespace
+    final setter = args.setter
+
+    final nsObject = library(identifier: libraryName, changelog: false)."$namespace"
+
+    return classNames.collect {
+        final instance = nsObject."$it".new()
+
+        if (setter) {
+            setter.call(it)
+        }
+
+        return instance
+    }
+}
